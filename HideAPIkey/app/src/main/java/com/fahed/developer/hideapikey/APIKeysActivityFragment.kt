4694f54fe0219dda06fc4a122df5b1dc3dfd499d
Fahed Hermoza Salcedo <fahed.hermoza@devartperu.com>
2020-05-31 23:51:31 -0500

    Added tutorial

--- HideAPIkey/app/src/main/java/com/fahed/developer/hideapikey/APIKeysActivityFragment.kt
+++ HideAPIkey/app/src/main/java/com/fahed/developer/hideapikey/APIKeysActivityFragment.kt
@@ -10,9 +10,21 @@ import kotlinx.android.synthetic.main.fragment_apikeys.*
 /**
  * Ocultando las keys en un archivo externo y recuperando con BuildConfigs
  * 1. Agregar API key="value" al archivo apikey.properties (creado en el root)
+ *
  * 2. Agregar una entrada a build.gradle (module: ) (2 tipos: file and value)
+ *
+ *      * Al ofuscarlo Proguard: No es visible el archivo BuildConfig.class (key Facebook)
+ *
+ *      BuildConfig: Al ofuscarlo y decompilarlo(JD-GUI) es visible el valor en el codigo: setText("551745000360889 - 551745000360889")
+ *
+ *      Value: Al ofuscarlo y decompilarlo(JD-GUI) no es visible el valor en el codigo: stringBuilder.append(a(2131689472));
+ *
+ *      * Al convertir a apk a .zip no existe el archivo apikey.properties
+ *
  * 3.Eliminar apikey.properties para subirlo a github
+ *
  *      Add apikey.properties a .gitignore
+ *
  * 3.1 Error al subirlo a Github, porque su archivo apikey.properties sigue siendo visible
  *      https://stackoverflow.com/a/11451731/6254339
  *          git rm -r --cached .
@@ -33,7 +45,7 @@ class APIKeysActivityFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        textViewFile.text = BuildConfig.API_KEY_FACEBOOK_APLICATION_ID +" - "+ BuildConfig.API_KEY_FACEBOOK_APLICATION_ID
+        //textViewFile.text = BuildConfig.API_KEY_FACEBOOK_APLICATION_ID +" - "+ BuildConfig.API_KEY_FACEBOOK_APLICATION_ID
         textViewValue.text = getString(R.string.API_KEY_FACEBOOK_APLICATION_ID) + " - "+ getString(R.string.API_KEY_FACEBOOK_APLICATION_ID)
     }
 }
